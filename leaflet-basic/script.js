35dffe8a5a4c73f78788cbe9fc27032b9b68f4df
Peter Beshai <peter.beshai@gmail.com>
2017-07-13 17:58:15 -0400

    Update to use DC as the location

--- leaflet-basic/script.js
+++ leaflet-basic/script.js
@@ -1,6 +1,6 @@
-var latitude = 42.3601;
-var longitude = -71.0589
-var zoom = 15;
+var latitude = 38.8932;
+var longitude = -77.0369;
+var zoom = 14;
 
 // intialize Leaflet on the #map element, centered on our lat/long and zoom level
 var map = L.map('map').setView([latitude, longitude], zoom);
@@ -9,7 +9,7 @@ var map = L.map('map').setView([latitude, longitude], zoom);
 // sign up for one here: https://www.mapbox.com/studio/account/tokens/
 var mapboxAccessToken = 'pk.eyJ1IjoicGV0ZXJib2NvdXAiLCJhIjoiY2o1Mnc2eDhvMDBpeTJ4cG5hdzFmNTFmeiJ9.r1oblW_7pK24AQFA_afkVA';
 
-// Mapbox has a number of classic tile set IDs available to choose from, or you
+// Mapbox has a number of tileset and styles available to choose from, or you
 // can make your own. (https://www.mapbox.com/api-documentation/#maps)
 // Preset ones include: mapbox.streets, mapbox.light, mapbox.dark, mapbox.satellite,
 //   mapbox.streets-satellite, mapbox.wheatpaste, mapbox.streets-basic,
