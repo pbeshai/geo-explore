9312b6748fb28a0759e0eceead0ce72254252c6b
Peter Beshai <peter.beshai@gmail.com>
2017-07-13 16:48:49 -0400

    Add leaflet basic

---
+++ leaflet-basic/script.js
@@ -0,0 +1,26 @@
+var latitude = 42.3601;
+var longitude = -71.0589
+var zoom = 15;
+
+// intialize Leaflet on the #map element, centered on our lat/long and zoom level
+var map = L.map('map').setView([latitude, longitude], zoom);
+
+// to use mapbox tiles, we need an access token.
+// sign up for one here: https://www.mapbox.com/studio/account/tokens/
+var mapboxAccessToken = 'pk.eyJ1IjoicGV0ZXJib2NvdXAiLCJhIjoiY2o1Mnc2eDhvMDBpeTJ4cG5hdzFmNTFmeiJ9.r1oblW_7pK24AQFA_afkVA';
+
+// Mapbox has a number of classic tile set IDs available to choose from, or you
+// can make your own. (https://www.mapbox.com/api-documentation/#maps)
+// Preset ones include: mapbox.streets, mapbox.light, mapbox.dark, mapbox.satellite,
+//   mapbox.streets-satellite, mapbox.wheatpaste, mapbox.streets-basic,
+//   mapbox.comic, mapbox.outdoors, mapbox.run-bike-hike, mapbox.pencil,
+//   mapbox.pirates, mapbox.emerald, mapbox.high-contrast
+var mapboxTilesetId = 'mapbox.streets';
+
+// add the tile layer to our map
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: mapboxTilesetId,
+    accessToken: mapboxAccessToken
+}).addTo(map);
