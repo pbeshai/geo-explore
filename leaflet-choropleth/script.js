3302decd546a520929e2d923e9440c18cda71bf5
Peter Beshai <peter.beshai@gmail.com>
2017-07-14 12:59:14 -0400

    Limit zoom levels

--- leaflet-choropleth/script.js
+++ leaflet-choropleth/script.js
@@ -20,7 +20,8 @@ var mapboxTilesetId = 'mapbox.light';
 // add the tile layer to our map
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 7,
+    minZoom: 3,
     id: mapboxTilesetId,
     accessToken: mapboxAccessToken
 }).addTo(map);
