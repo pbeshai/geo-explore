2337ba72076db759284eb604c3f8fbc44fc40cde
Peter Beshai <peter.beshai@gmail.com>
2017-07-14 13:13:12 -0400

    Add legend to choropleth

--- leaflet-choropleth/README.md
+++ leaflet-choropleth/README.md
@@ -1,1 +1,1 @@
-Shows how to draw a leaflet slippy choropleth map with hover and click functionality as well as an information panel.
+Shows how to draw a leaflet slippy choropleth map with hover and click functionality as well as an information panel and legend.
--- leaflet-choropleth/script.js
+++ leaflet-choropleth/script.js
@@ -1,3 +1,4 @@
+// heavily inspired by http://leafletjs.com/examples/choropleth/
 var latitude = 37.0902;
 var longitude = -95.7129;
 var zoom = 4;
@@ -26,6 +27,14 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: mapboxAccessToken
 }).addTo(map);
 
+// define color out here so it can be shared by the legend and the choropleth
+// create a color scale to use for coloring in states based on density
+var colorScale = d3.scaleThreshold()
+  .domain([0, 10, 20, 50, 100, 200, 500, 1000])
+  .range(d3.schemeYlGnBu[9]);
+
+
+// Info Panel ----------------------------
 // a global to hold our custom information control showing details about states
 // Note this is styled via CSS
 var info = L.control();
@@ -44,6 +53,28 @@ info.update = function (props) {
 };
 
 info.addTo(map);
+
+
+// Legend ----------------------------
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+      labels = [];
+
+  // loop through our density intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+      '<i style="background:' + colorScale(grades[i] + 1) + '"></i> ' +
+      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+
+  return div;
+};
+
+legend.addTo(map);
+
 
 // a global to hold our geojson layer
 var geojson;
@@ -93,28 +124,23 @@ function onEachFeature(feature, layer) {
   });
 }
 
+// color in each state based on density
+function style(feature) {
+  return {
+    fillColor: colorScale(feature.properties.density),
+    weight: 1,
+    opacity: 0.5,
+    color: 'white',
+    fillOpacity: 0.8,
+  };
+}
+
 function display(error, statesGeo) {
   if (error) {
     console.error(error);
     return;
   }
   console.log('statesGeo', statesGeo);
-
-  // create a color scale to use for coloring in states based on density
-  var colorScale = d3.scaleThreshold()
-    .domain([1, 10, 50, 200, 500, 1000])
-    .range(d3.schemeYlGnBu[7]);
-
-  // color in each state based on density
-  function style(feature) {
-    return {
-      fillColor: colorScale(feature.properties.density),
-      weight: 1,
-      opacity: 0.5,
-      color: 'white',
-      fillOpacity: 0.8,
-    };
-  }
 
   // add the states GeoJSON data to the map
   geojson = L.geoJson(statesGeo, { style: style, onEachFeature: onEachFeature }).addTo(map);
