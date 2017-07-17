002e687e2f8cd98cca38511192207034c70c5e22
Peter Beshai <peter.beshai@gmail.com>
2017-07-17 16:19:01 -0400

    Revert back to using DC

--- leaflet-basic/script.js
+++ leaflet-basic/script.js
@@ -1,8 +1,6 @@
 var latitude = 38.8932;
 var longitude = -77.0369;
 var zoom = 14;
-var latitude = 42.3601;
-var longitude = -71.0589;
 
 // intialize Leaflet on the #map element, centered on our lat/long and zoom level
 var map = L.map('map').setView([latitude, longitude], zoom);
